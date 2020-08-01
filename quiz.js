6870b08684a2acb6c234f9edd50e965d84020109
bosshogg86 <jeffhogg86@gmail.com>
2020-07-31 20:01:53 -0700

    start button hide

--- quiz.js
+++ quiz.js
@@ -1,20 +1,29 @@
 // Variable declarations
-
+const startButton = document.getElementById("start-btn");
+const questionContainer = document.getElementById("question-container");
+let score = 0;
+let questions = [
+    {
+        
+    }
+]
 // Functions
 function startQuiz() {
-    let counter = 60;
-    var interval = setInterval(function() {
+  startButton.classList.add("hide");
+  let counter = 60;
+  let interval = setInterval(function () {
     counter--;
     if (counter <= 0) {
-     		clearInterval(interval);
-      	$('#timer').html("Times Up!");  
-        return;
-    }else{
-    	$('#seconds').text(counter);
+      clearInterval(interval);
+      $("#timer").html("Times Up!");
+      return;
+    } else {
+      $("#seconds").text(counter);
       console.log("Timer --> " + counter);
     }
-}, 1000);
+  }, 1000);
 }
 
+function setNextQuestion() {}
 // Event listeners
 document.getElementById("start-btn").addEventListener("click", startQuiz);
