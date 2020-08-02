92b67a1abe52f162caa632b4d54f7de9f3c668be
bosshogg86 <jeffhogg86@gmail.com>
2020-08-01 18:23:23 -0700

    added button ids, parent click listener

--- quiz.js
+++ quiz.js
@@ -1,35 +1,36 @@
 // Variable declarations
 const home = document.getElementById("home");
+const timer = document.getElementById("timer");
 const startButton = document.getElementById("start-btn");
 const questionContainer = document.getElementById("question-container");
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementByClassName("choice-text"));
+const choices = Array.from(document.querySelector(".choice-text"));
 let score = 0;
 let questions = [
     {
         question: 'Who invented JavaScript?',
-        choice1: '1. Elon Musk',
-        choice2: '2. Bill Gates',
-        choice3: '3. Mark Zuckerberg',
-        choice4: '4. Brendan Eich',
-        answer: 4,
+        choice0: '1. Elon Musk',
+        choice1: '2. Bill Gates',
+        choice2: '3. Mark Zuckerberg',
+        choice3: '4. Brendan Eich',
+        answer: 3,
     },
     {
         question:
             "How many days did it take to write JavaScript?",
-        choice1: "10 days",
-        choice2: "2 weeks",
-        choice3: "2 months",
-        choice4: "2 years",
-        answer: 1,
+        choice0: "10 days",
+        choice1: "2 weeks",
+        choice2: "2 months",
+        choice3: "2 years",
+        answer: 0,
     },
     {
         question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
+        choice0: "msgBox('Hello World');",
+        choice1: "alertBox('Hello World');",
+        choice2: "msg('Hello World');",
+        choice3: "alert('Hello World');",
+        answer: 3,
     },
 ];
 
@@ -37,6 +38,7 @@ let questions = [
 function startQuiz() {
   home.classList.add("hide");
   questionContainer.classList.remove("hide");
+  timer.classList.remove("hide");
   let counter = 60;
   let interval = setInterval(function () {
     counter--;
@@ -51,6 +53,10 @@ function startQuiz() {
   }, 1000);
 }
 
-function setNextQuestion() {}
+function nextQuestion() {
+ 
+}
+
 // Event listeners
 document.getElementById("start-btn").addEventListener("click", startQuiz);
+document.getElementById("question-container").addEventListener("click", nextQuestion);
